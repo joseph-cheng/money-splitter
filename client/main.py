cd5a67dcc1b4818efd4c965fa2f45c53014ad68b
Joe Cheng <joe.cheng2001@gmail.com>
2022-08-26 16:07:06 +0200

    added new style for tkinter

--- client/main.py
+++ client/main.py
@@ -1,10 +1,15 @@
 from client.client import Client
 from client.gui.app import App
+from tkinter import ttk
 
 
 
 
 if __name__ == "__main__":
 
+
+
     app = App()
+    app.style = ttk.Style()
+    app.style.theme_use("clam")
     app.mainloop()
